9f79dcd782e2335e28f4824ef5c8684ad0bb5040
Fred Metrich <frederic.metrich@live.fr>
2022-08-24 10:58:32 +0200

    Shipped

--- Mk2_3phase_RFdatalog_temp/Mk2_3phase_RFdatalog_temp.ino
+++ Mk2_3phase_RFdatalog_temp/Mk2_3phase_RFdatalog_temp.ino
@@ -167,7 +167,7 @@ constexpr uint8_t DATALOG_PERIOD_IN_SECONDS{5}; /**< Period of datalogging in se
 // powerCal is the RECIPR0CAL of the power conversion rate. A good value
 // to start with is therefore 1/20 = 0.05 (Watts per ADC-step squared)
 //
-constexpr float f_powerCal[NO_OF_PHASES]{0.05000f, 0.05000f, 0.05000f};
+constexpr float f_powerCal[NO_OF_PHASES]{0.05371f, 0.05373f, 0.05282f};
 //
 // f_phaseCal is used to alter the phase of the voltage waveform relative to the current waveform.
 // The algorithm interpolates between the most recent pair of voltage samples according to the value of f_phaseCal.
@@ -188,7 +188,7 @@ constexpr int16_t p_phaseCal{8};   /**< to speed up math (i_phaseCal = 1 << p_ph
 // For datalogging purposes, f_voltageCal has been added too. Because the range of ADC values is
 // similar to the actual range of volts, the optimal value for this cal factor is likely to be
 // close to unity.
-constexpr float f_voltageCal[NO_OF_PHASES]{1.03f, 1.03f, 1.03f} /*{1.03f, 1.03f, 1.03f}*/; /**< compared with Fluke 77 meter */
+constexpr float f_voltageCal[NO_OF_PHASES]{0.803f, 0.803f, 0.803f}; /**< compared with Fluke 77 meter */
 //--------------------------------------------------------------------------------------------------
 
 //--------------------------------------------------------------------------------------------------
