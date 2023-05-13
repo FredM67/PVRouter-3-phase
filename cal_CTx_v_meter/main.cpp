5deae48ed2188defa20c924d96c5f89b67d93df1
Frédéric Metrich <45318189+FredM67@users.noreply.github.com>
2023-05-13 22:59:11 +0200

    Fix links

--- cal_CTx_v_meter/main.cpp
+++ cal_CTx_v_meter/main.cpp
@@ -1,6 +1,6 @@
 
 static_assert(__cplusplus >= 201703L, "**** Please define 'gnu++17' in 'platform.txt' ! ****");
-static_assert(__cplusplus >= 201703L, "See also : https://github.com/FredM67/PVRouter-3-phase/blob/Laclare/Mk2_3phase_RFdatalog_temp/Readme.md");
+static_assert(__cplusplus >= 201703L, "See also : https://github.com/FredM67/PVRouter-3-phase/blob/main/Mk2_3phase_RFdatalog_temp/Readme.md");
 
 #include <Arduino.h>  // may not be needed, but it's probably a good idea to include this
 
