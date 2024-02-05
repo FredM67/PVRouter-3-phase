ada4d0615861f4f068555fce497bbdb65f217e80
Frédéric Metrich <45318189+FredM67@users.noreply.github.com>
2024-02-05 15:46:55 +0100

    Final

--- Mk2_3phase_RFdatalog_temp/Mk2_3phase_RFdatalog_temp.ino.cpp
+++
@@ -1,6 +0,0 @@
-# 1 "C:\\Users\\frede\\AppData\\Local\\Temp\\tmpf2lv1g07"
-#include <Arduino.h>
-# 1 "D:/Git/IoT/PVRouter-3-phase/Mk2_3phase_RFdatalog_temp/Mk2_3phase_RFdatalog_temp.ino"
-# 148 "D:/Git/IoT/PVRouter-3-phase/Mk2_3phase_RFdatalog_temp/Mk2_3phase_RFdatalog_temp.ino"
-static_assert(__cplusplus >= 201703L, "**** Please define 'gnu++17' in 'platform.txt' ! ****");
-static_assert(__cplusplus >= 201703L, "See also : https://github.com/FredM67/PVRouter-3-phase/blob/main/Mk2_3phase_RFdatalog_temp/Readme.md");
