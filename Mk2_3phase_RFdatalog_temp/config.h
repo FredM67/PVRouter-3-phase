66719b1fe1a4c9e7277609e0e6a5a5ecf82d3ee2
Frédéric Metrich <frederic.metrich@live.fr>
2025-01-31 13:32:41 +0100

    Update watchdog pin configuration and adjust post-transition max count for load management

--- Mk2_3phase_RFdatalog_temp/config.h
+++ Mk2_3phase_RFdatalog_temp/config.h
@@ -49,7 +49,7 @@ inline constexpr RotationModes PRIORITY_ROTATION{ RotationModes::OFF }; /**< set
 inline constexpr bool OVERRIDE_PIN_PRESENT{ false };                    /**< set it to 'true' if there's a override pin */
 #endif
 
-inline constexpr bool WATCHDOG_PIN_PRESENT{ false }; /**< set it to 'true' if there's a watch led */
+inline constexpr bool WATCHDOG_PIN_PRESENT{ true }; /**< set it to 'true' if there's a watch led */
 inline constexpr bool RELAY_DIVERSION{ false };      /**< set it to 'true' if a relay is used for diversion */
 inline constexpr bool DUAL_TARIFF{ false };          /**< set it to 'true' if there's a dual tariff each day AND the router is connected to the billing meter */
 
@@ -79,7 +79,7 @@ inline constexpr uint8_t dualTariffPin{ 0xff }; /**< for 3-phase PCB, off-peak t
 inline constexpr uint8_t diversionPin{ 0xff };  /**< if LOW, set diversion on standby */
 inline constexpr uint8_t rotationPin{ 0xff };   /**< if LOW, trigger a load priority rotation */
 inline constexpr uint8_t forcePin{ 0xff };      /**< for 3-phase PCB, force pin */
-inline constexpr uint8_t watchDogPin{ 0xff };   /**< watch dog LED */
+inline constexpr uint8_t watchDogPin{ 4 };   /**< watch dog LED */
 
 inline constexpr RelayEngine relays{ { { 0xff, 1000, 200, 1, 1 } } }; /**< config for relay diversion, see class definition for defaults and advanced options */
 
@@ -90,9 +90,6 @@ inline constexpr int16_t iTemperatureThreshold{ 100 }; /**< the temperature thre
 
 inline constexpr TemperatureSensing temperatureSensing{ 0xff,
                                                         { { 0x28, 0xBE, 0x41, 0x6B, 0x09, 0x00, 0x00, 0xA4 },
-                                                          { 0x28, 0xED, 0x5B, 0x6A, 0x09, 0x00, 0x00, 0x9D },
-                                                          { 0x28, 0xDB, 0x6D, 0x6A, 0x09, 0x00, 0x00, 0xDA },
-                                                          { 0x28, 0x59, 0x1F, 0x6A, 0x09, 0x00, 0x00, 0xB0 },
                                                           { 0x28, 0x1B, 0xD7, 0x6A, 0x09, 0x00, 0x00, 0xB7 } } }; /**< list of temperature sensor Addresses */
 
 inline constexpr uint32_t ROTATION_AFTER_CYCLES{ 8UL * 3600UL * SUPPLY_FREQUENCY }; /**< rotates load priorities after this period of inactivity */
