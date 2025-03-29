f01a6fc24fba441219ab80ccef9f981514a3617b
FredM67 <45318189+FredM67@users.noreply.github.com>
2025-03-29 13:20:32 +0000

    Deploying to gh-pages from @ FredM67/PVRouter-3-phase@3c836719896d2e0670bd1e238d9ef7a96c7f024c 🚀

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['calcbuffersize_0',['calcBufferSize',['../teleinfo_8h.html#a820a2f759b537f10c0543d9612390e8c',1,'teleinfo.h']]],
-  ['calculatechecksum_1',['calculateChecksum',['../classTeleInfo.html#a6b27a06e0b134203f413af0b2d5c41c0',1,'TeleInfo']]],
+  ['calculatechecksum_1',['calculateChecksum',['../classTeleInfo.html#a1f42b9e6aa9c3169b3ec469149e693a1',1,'TeleInfo']]],
   ['check_5fload_5fpriorities_2',['check_load_priorities',['../validation_8h.html#a97da13005cbcf80e3bb29f69a46139ac',1,'validation.h']]],
   ['check_5fpins_3',['check_pins',['../validation_8h.html#a07d77671cb50e935bf9015eb1bef3bcc',1,'validation.h']]],
   ['check_5frelay_5fpins_4',['check_relay_pins',['../validation_8h.html#a60096a6d3a2fc387b51a01b6ff8c53d2',1,'validation.h']]],
