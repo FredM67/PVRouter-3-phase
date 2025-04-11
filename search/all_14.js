75cf69f7148215e8dd8cd71f44ee600b1bea79c4
FredM67 <45318189+FredM67@users.noreply.github.com>
2025-04-11 06:33:17 +0000

    Deploying to gh-pages from @ FredM67/PVRouter-3-phase@8cba6156f93cada3316b5df89056a7e58d58b93a 🚀

--- search/all_14.js
+++ search/all_14.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['validation_2eh_0',['validation.h',['../validation_8h.html',1,'']]],
   ['value_1',['value',['../structintegral__constant.html#a898163d4533374f6993a6c2e2116443a',1,'integral_constant::value'],['../classis__base__of.html#af5099c0b8b7ac33766bbc071e8df525b',1,'is_base_of::value'],['../structis__class.html#a8e480c366229d83043464856beaef26f',1,'is_class::value'],['../structis__convertible.html#ae961431c0a8dc3506828194f8ec1c684',1,'is_convertible::value'],['../structis__enum.html#aecdd48955dfb72b90880aafdec94e2db',1,'is_enum::value']]],
-  ['void_5ft_2',['void_t',['../void__t_8hpp.html#af0794fbfcd2813c921fb6d9ffc55ec90',1,'void_t.hpp']]],
-  ['void_5ft_2ehpp_3',['void_t.hpp',['../void__t_8hpp.html',1,'']]],
-  ['vrms_5fl_5fx100_4',['Vrms_L_x100',['../classPayloadTx__struct.html#a057a8416a916513fd1c0667dbf08713f',1,'PayloadTx_struct']]]
+  ['version_2eh_2',['version.h',['../version_8h.html',1,'']]],
+  ['void_5ft_3',['void_t',['../void__t_8hpp.html#af0794fbfcd2813c921fb6d9ffc55ec90',1,'void_t.hpp']]],
+  ['void_5ft_2ehpp_4',['void_t.hpp',['../void__t_8hpp.html',1,'']]],
+  ['vrms_5fl_5fx100_5',['Vrms_L_x100',['../classPayloadTx__struct.html#a057a8416a916513fd1c0667dbf08713f',1,'PayloadTx_struct']]]
 ];
--- search/all_4.js
+++ search/all_4.js
@@ -8,7 +8,7 @@ var searchData=
   ['check_5frelay_5fpins_5',['check_relay_pins',['../validation_8h.html#a60096a6d3a2fc387b51a01b6ff8c53d2',1,'validation.h']]],
   ['checkdiversiononoff_6',['checkDiversionOnOff',['../group__GeneralProcessing.html#ga31f3d562faae6b598324b56299d74f7d',1,'main.cpp']]],
   ['clear_7',['clear',['../classmovingAvg.html#adc157cfdb883805b88f3979cebb72de7',1,'movingAvg']]],
-  ['commit_5fhash_8',['COMMIT_HASH',['../utils_8h.html#a2c55f8693f5917de9217fba7c72ce663',1,'utils.h']]],
+  ['commit_5fhash_8',['COMMIT_HASH',['../utils_8h.html#a2c55f8693f5917de9217fba7c72ce663',1,'COMMIT_HASH:&#160;utils.h'],['../version_8h.html#a2c55f8693f5917de9217fba7c72ce663',1,'COMMIT_HASH:&#160;version.h']]],
   ['conditional_9',['conditional',['../structconditional.html',1,'']]],
   ['conditional_2ehpp_10',['conditional.hpp',['../conditional_8hpp.html',1,'']]],
   ['conditional_3c_20false_2c_20truetype_2c_20falsetype_20_3e_11',['conditional&lt; false, TrueType, FalseType &gt;',['../structconditional_3_01false_00_01TrueType_00_01FalseType_01_4.html',1,'']]],
@@ -27,5 +27,6 @@ var searchData=
   ['copyof_5fsump_5fatsupplypoint_24',['copyOf_sumP_atSupplyPoint',['../processing_8h.html#a92487f63c679b2125aa51fddd1549836',1,'processing.h']]],
   ['countloadon_25',['countLoadON',['../processing_8cpp.html#a2f61fd181a2806e678e5473eaad03891',1,'processing.cpp']]],
   ['cr_26',['CR',['../classTeleInfo.html#a710390df1d82afc54addcd2edb3405ae',1,'TeleInfo']]],
-  ['critical_20functions_27',['Time-critical functions',['../group__TimeCritical.html',1,'']]]
+  ['critical_20functions_27',['Time-critical functions',['../group__TimeCritical.html',1,'']]],
+  ['current_5ftime_28',['CURRENT_TIME',['../version_8h.html#a61eabc8e73414cfe55910effa8bcc4ef',1,'version.h']]]
 ];
