8cba6156f93cada3316b5df89056a7e58d58b93a
Frédéric Metrich <frederic.metrich@live.fr>
2025-04-11 08:32:34 +0200

    Add version.h header file for project metadata definition

---
+++ Mk2_3phase_RFdatalog_temp/version.h
@@ -0,0 +1,9 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define PROJECT_PATH "Mk2_3phase_RFdatalog_temp"
+#define CURRENT_TIME "N/A"
+#define BRANCH_NAME "N/A"
+#define COMMIT_HASH "N/A"
+
+#endif // VERSION_H
