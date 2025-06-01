2c7c3ecfa6c068ce01147333e8e971b8799bdabf
FredM67 <45318189+FredM67@users.noreply.github.com>
2025-06-01 16:49:18 +0000

    Deploying to gh-pages from @ FredM67/PVRouter-3-phase@345190812eb723490f41704ae47786ec5ceb5cc9 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -1,13 +1,14 @@
 var searchData=
 [
   ['a_203_20phase_20photovoltaic_20router_20diverter_0',['A 3-phase photovoltaic router/diverter',['../index.html',1,'']]],
-  ['absenceofdivertedenergycountinseconds_1',['absenceOfDivertedEnergyCountInSeconds',['../namespaceShared.html#ae10f34dba364ce75e88b0c261bc6dab8',1,'Shared']]],
-  ['activeload_2',['activeLoad',['../processing_8cpp.html#ae3ad049e6ee90e4eacc823d1387f02e0',1,'processing.cpp']]],
-  ['addr_3',['addr',['../structDeviceAddress.html#ae90422657105a8cff63931e274f2d391',1,'DeviceAddress']]],
-  ['addvalue_4',['addValue',['../classEWMA__average.html#a208b9b3d82809466b4d90aedb3c7e4db',1,'EWMA_average::addValue()'],['../classmovingAvg.html#aa0d0d8ba97b9d3fdfdbcf3a0b8d8f0f5',1,'movingAvg::addValue()']]],
-  ['alpha_5',['alpha',['../calibration_8h.html#a19b3d4f09bab83f43bd3a3ed5295bddb',1,'calibration.h']]],
-  ['anti_5fflicker_6',['ANTI_FLICKER',['../types_8h.html#aae913bd32d3b848f4a5ba5b9d32e9a1fa086599a6d23b3982c7ebfeb833fdd6c0',1,'types.h']]],
-  ['arg1_5ftype_7',['arg1_type',['../structfunction__traits_3_01ReturnType_07_5_08_07Arg1_08_4.html#a38f967cfcb5daa4bbad2bd62e6a81940',1,'function_traits&lt; ReturnType(*)(Arg1)&gt;::arg1_type'],['../structfunction__traits_3_01ReturnType_07_5_08_07Arg1_00_01Arg2_08_4.html#a4deb6f7e70bf678c274b69f6e0d70138',1,'function_traits&lt; ReturnType(*)(Arg1, Arg2)&gt;::arg1_type']]],
-  ['arg2_5ftype_8',['arg2_type',['../structfunction__traits_3_01ReturnType_07_5_08_07Arg1_00_01Arg2_08_4.html#a63022e1fee5060dd3a47065b6cb75599',1,'function_traits&lt; ReturnType(*)(Arg1, Arg2)&gt;']]],
-  ['auto_9',['AUTO',['../types_8h.html#a06b890c4a964752df814257dc1f13073ae1f2d5134ed2543d38a0de9751cf75d9',1,'types.h']]]
+  ['absenceofdivertedenergycountinmc_1',['absenceOfDivertedEnergyCountInMC',['../processing_8cpp.html#a16fdc76855b18530d79f3b3541a3c0ca',1,'processing.cpp']]],
+  ['absenceofdivertedenergycountinseconds_2',['absenceOfDivertedEnergyCountInSeconds',['../namespaceShared.html#ae10f34dba364ce75e88b0c261bc6dab8',1,'Shared']]],
+  ['activeload_3',['activeLoad',['../processing_8cpp.html#ae3ad049e6ee90e4eacc823d1387f02e0',1,'processing.cpp']]],
+  ['addr_4',['addr',['../structDeviceAddress.html#ae90422657105a8cff63931e274f2d391',1,'DeviceAddress']]],
+  ['addvalue_5',['addValue',['../classEWMA__average.html#a208b9b3d82809466b4d90aedb3c7e4db',1,'EWMA_average::addValue()'],['../classmovingAvg.html#aa0d0d8ba97b9d3fdfdbcf3a0b8d8f0f5',1,'movingAvg::addValue()']]],
+  ['alpha_6',['alpha',['../calibration_8h.html#a19b3d4f09bab83f43bd3a3ed5295bddb',1,'calibration.h']]],
+  ['anti_5fflicker_7',['ANTI_FLICKER',['../types_8h.html#aae913bd32d3b848f4a5ba5b9d32e9a1fa086599a6d23b3982c7ebfeb833fdd6c0',1,'types.h']]],
+  ['arg1_5ftype_8',['arg1_type',['../structfunction__traits_3_01ReturnType_07_5_08_07Arg1_08_4.html#a38f967cfcb5daa4bbad2bd62e6a81940',1,'function_traits&lt; ReturnType(*)(Arg1)&gt;::arg1_type'],['../structfunction__traits_3_01ReturnType_07_5_08_07Arg1_00_01Arg2_08_4.html#a4deb6f7e70bf678c274b69f6e0d70138',1,'function_traits&lt; ReturnType(*)(Arg1, Arg2)&gt;::arg1_type']]],
+  ['arg2_5ftype_9',['arg2_type',['../structfunction__traits_3_01ReturnType_07_5_08_07Arg1_00_01Arg2_08_4.html#a63022e1fee5060dd3a47065b6cb75599',1,'function_traits&lt; ReturnType(*)(Arg1, Arg2)&gt;']]],
+  ['auto_10',['AUTO',['../types_8h.html#a06b890c4a964752df814257dc1f13073ae1f2d5134ed2543d38a0de9751cf75d9',1,'types.h']]]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['absenceofdivertedenergycountinseconds_0',['absenceOfDivertedEnergyCountInSeconds',['../namespaceShared.html#ae10f34dba364ce75e88b0c261bc6dab8',1,'Shared']]],
-  ['activeload_1',['activeLoad',['../processing_8cpp.html#ae3ad049e6ee90e4eacc823d1387f02e0',1,'processing.cpp']]],
-  ['addr_2',['addr',['../structDeviceAddress.html#ae90422657105a8cff63931e274f2d391',1,'DeviceAddress']]],
-  ['alpha_3',['alpha',['../calibration_8h.html#a19b3d4f09bab83f43bd3a3ed5295bddb',1,'calibration.h']]]
+  ['absenceofdivertedenergycountinmc_0',['absenceOfDivertedEnergyCountInMC',['../processing_8cpp.html#a16fdc76855b18530d79f3b3541a3c0ca',1,'processing.cpp']]],
+  ['absenceofdivertedenergycountinseconds_1',['absenceOfDivertedEnergyCountInSeconds',['../namespaceShared.html#ae10f34dba364ce75e88b0c261bc6dab8',1,'Shared']]],
+  ['activeload_2',['activeLoad',['../processing_8cpp.html#ae3ad049e6ee90e4eacc823d1387f02e0',1,'processing.cpp']]],
+  ['addr_3',['addr',['../structDeviceAddress.html#ae90422657105a8cff63931e274f2d391',1,'DeviceAddress']]],
+  ['alpha_4',['alpha',['../calibration_8h.html#a19b3d4f09bab83f43bd3a3ed5295bddb',1,'calibration.h']]]
 ];
