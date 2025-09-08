7f2e6a6d19cdc2d7c504dbf8b096c46a9bf17a66
Frédéric Metrich <frederic.metrich@live.fr>
2025-09-08 18:45:42 +0200

    refactor: Remove unused temperature sensor addresses for cleaner configuration

--- Mk2_3phase_RFdatalog_temp/config.h
+++ Mk2_3phase_RFdatalog_temp/config.h
@@ -141,9 +141,6 @@ inline constexpr int16_t iTemperatureThreshold{ 100 }; /**< the temperature thre
 
 inline constexpr TemperatureSensing temperatureSensing{ unused_pin,
                                                         { { 0x28, 0xBE, 0x41, 0x6B, 0x09, 0x00, 0x00, 0xA4 },
-                                                          { 0x28, 0xED, 0x5B, 0x6A, 0x09, 0x00, 0x00, 0x9D },
-                                                          { 0x28, 0xDB, 0x6D, 0x6A, 0x09, 0x00, 0x00, 0xDA },
-                                                          { 0x28, 0x59, 0x1F, 0x6A, 0x09, 0x00, 0x00, 0xB0 },
                                                           { 0x28, 0x1B, 0xD7, 0x6A, 0x09, 0x00, 0x00, 0xB7 } } }; /**< list of temperature sensor Addresses */
 
 inline constexpr uint32_t ROTATION_AFTER_SECONDS{ 8UL * 3600UL }; /**< rotates load priorities after this period of inactivity */
